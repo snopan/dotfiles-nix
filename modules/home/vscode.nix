d3b39c99ebe10c8115900663c1d7c067cf88eece
snopan <>
2026-09-11 18:02:58 +1000

    use extension with custom detail

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -16,13 +16,18 @@
             rust-lang.rust-analyzer
             esbenp.prettier-vscode
             firsttris.vscode-jest-runner
-            openai.chatgpt
         ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         {
             name = "opencode";
             publisher = "sst-dev";
             version = "0.0.13";
             sha256 = "sha256-6adXUaoh/OP5yYItH3GAQ7GpupfmTGaxkKP6hYUMYNQ="; 
+        }
+        {
+            name = "chatgpt";
+            publisher = "openai";
+            version = "26.908.31748";
+            sha256 = "sha256-sha256-etDbaBqJfIjbjY5TGOwnA4JddGaPx/7rWkE2gnSV4G8=";
         }
         ];
 
